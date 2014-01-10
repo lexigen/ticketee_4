0188642455b5e4516f042c0d261b1236be9dd9d9
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2014-01-10 17:01:44 +0300

    Implement displaying a list of relevant tickets.

---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'Viewing Tickets' do
+  before do
+    textmate_2 = FactoryGirl.create(:project, name: "TextMate 2")
+    FactoryGirl.create(:ticket, title: "Make it shiny!", description: "Gradients! Starbursts! Oh my!", project: textmate_2)
+
+    internet_explorer = FactoryGirl.create(:project, name: "Internet Explorer")
+    FactoryGirl.create(:ticket, title: "Standards compliance", description: "Isn't a joke.", project: internet_explorer)
+
+    visit '/'
+  end
+
+  scenario "Viewing tickets for a given project" do
+    click_link "TextMate 2"
+
+    expect(page).to have_content("Make it shiny!")
+    expect(page).to_not have_content("Standards compliance")
+
+    click_link "Make it shiny!"
+    within("#ticket h2") do
+      expect(page).to have_content("Make it shiny!")
+    end
+
+    expect(page).to have_content("Gradients! Starbursts! Oh my!")
+  end
+end
---
+++ spec/support/factories/ticket_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :ticket do
+    title "Example Project"
+    description "Some long description"
+  end
+end 
