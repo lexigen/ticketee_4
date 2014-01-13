b99bb25c96cad1c1e21540a50710229985b38759
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2014-01-13 13:30:41 +0300

    Implement profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -16,9 +18,30 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile has been updated."
+      redirect_to projects_path
+    else
+      flash[:alert] = "Profile has not been updated."
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The user you were looking" +
+    " for could not be found."
+    redirect_to projects_path
   end
 end
