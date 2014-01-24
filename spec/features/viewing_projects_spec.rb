3013da9c76b2d6f8dfdbb1097cd24d72855fa9a0
Aleksei Maridashvili <aleksei.maridashvili@altoros.com>
2014-01-24 10:29:22 +0300

    Don’t show projects that a user doesn't have permission to see

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -5,12 +5,14 @@ feature "Viewing projects" do
   let!(:project) { FactoryGirl.create(:project) }
 
   before do
+    FactoryGirl.create(:project, name: "Hidden")
     sign_in_as!(user)
     define_permission!(user, :view, project)
   end
 
   scenario "Listing all projects" do
     visit '/'
+    expect(page).to_not have_content("Hidden")
     click_link project.name
     
     expect(page.current_url).to eql(project_url(project))
